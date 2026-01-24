32df52840f383baa3ff001d799a1453f2262b269
Corinna Heinze <hei.cor@web.de>
2026-01-24 12:25:56 +0100

    feature/feedback: add Incentive

--- survey_corinna_heinze/closing/__init__.py
+++ survey_corinna_heinze/closing/__init__.py
@@ -1,3 +1,5 @@
+import re
+
 from otree.api import *
 
 
@@ -16,11 +18,37 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    contact_email = models.StringField(
+        blank=True,
+        label="Email address (optional, for voucher delivery only)"
+    )
 
 
 class Closing(Page):
-    pass
+    form_model = "player"
+    form_fields = ["contact_email"]
+
+    @staticmethod
+    def error_message(player, values):
+        email = values.get("contact_email")
+
+        # Case 1: Submit pressed without email
+        if not email:
+            return (
+                "Providing an email address is optional. "
+                "However, if you wish to participate in the voucher incentive, "
+                "please enter an email address. "
+                "If you do not wish to participate in the incentive, "
+                "you may simply close this page without submitting."
+            )
+
+        # Case 2: Email entered but invalid format
+        email_regex = r"^[^@\s]+@[^@\s]+\.[^@\s]+$"
+        if not re.match(email_regex, email):
+            return (
+                "Please enter a valid email address (for example: name@example.com). "
+                "The address you entered does not appear to be valid."
+            )
 
 
 page_sequence = [Closing]
