d4a04f81b6b9d41f6ad64184b235d8fd5f65e512
Corinna Heinze <hei.cor@web.de>
2026-02-13 13:58:54 +0100

    Revert "main: remove shap value check"
    This reverts commit 9eaa59cf3f995217099c4c9ef5637ca8d6cc525f.

--- survey_corinna_heinze/checks/__init__.py
+++ survey_corinna_heinze/checks/__init__.py
@@ -27,8 +27,19 @@ class Player(BasePlayer):
         widget=widgets.RadioSelect,
     )
 
+    check_model_understanding = models.IntegerField(
+        label="According to the explanation, which factors had the strongest influence on the AI’s assessment across cases?",
+        choices=[
+            [1, "Monthly Income"],
+            [2, "Interest Rate"],
+            [3, "Age"],
+        ],
+        widget=widgets.RadioSelect,
+    )
+
     # attempt counters
     attempts_decision_authority = models.IntegerField(initial=0)
+    attempts_model_understanding = models.IntegerField(initial=0)
 
     # exit flag
     failed_checks = models.BooleanField(initial=False)
@@ -37,7 +48,8 @@ class Player(BasePlayer):
 class Checks(Page):
     form_model = "player"
     form_fields = [
-        "check_decision_authority"
+        "check_decision_authority",
+        "check_model_understanding",
     ]
 
     @staticmethod
@@ -56,6 +68,20 @@ class Checks(Page):
 
             errors["check_decision_authority"] = (
                 "This answer is incorrect. Please read the instructions again and try once more."
+            )
+
+        # --- Question 2 ---
+        if values["check_model_understanding"] != 2:
+            attempts = player.field_maybe_none("attempts_model_understanding") or 0
+            attempts += 1
+            player.attempts_model_understanding = attempts
+
+            if attempts > 1:
+                player.failed_checks = True
+                return None
+
+            errors["check_model_understanding"] = (
+                "This answer is incorrect. Please review the explanation and try once more."
             )
 
         return errors
